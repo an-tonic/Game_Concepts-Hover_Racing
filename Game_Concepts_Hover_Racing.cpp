812eba42d7fd291797e1633892c926976dee0595
an-tonic <aserdyukov@uclan.ac.uk>
2023-01-27 16:59:50 +0200

    First commit, sky, an arch. camera

--- Game_Concepts_Hover_Racing.cpp
+++ Game_Concepts_Hover_Racing.cpp
@@ -1,7 +1,19 @@
 // Game_Concepts_Hover_Racing.cpp: A program using the TL-Engine
 
 #include <TL-Engine.h>	// TL-Engine include file and namespace
+#include <iostream>
+
 using namespace tle;
+using namespace std;
+
+
+//Constants
+
+
+//Functions
+
+
+
 
 void main()
 {
@@ -10,9 +22,18 @@ void main()
 	myEngine->StartWindowed();
 
 	// Add default folder for meshes and other media
-	myEngine->AddMediaFolder( "C:\\ProgramData\\TL-Engine\\Media" );
+	myEngine->AddMediaFolder( "C:\\Users\\aserdyukov\\source\\repos\\Game_Concepts_Hover_Racing\\Assesment 2 Resources" );
+	//myEngine->AddMediaFolder("./Assessment 2 Media");
 
 	/**** Set up your scene here ****/
+	ICamera* myCamera = myEngine->CreateCamera(kFPS);
+
+
+	IMesh* skyMesh = myEngine->LoadMesh("Skybox 07.x");
+	IModel* skyModel = skyMesh->CreateModel(0, -960, 0);
+
+	IMesh* archMesh = myEngine->LoadMesh("Checkpoint.x");
+	IModel* archModel = archMesh->CreateModel(0, 0, 0);
 
 
 	// The main game loop, repeat until engine is stopped
