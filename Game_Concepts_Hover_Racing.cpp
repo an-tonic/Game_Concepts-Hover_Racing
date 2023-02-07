318a3d2681a5deea649510f7789dd3c48b291585
an-tonic <aserdyukov@uclan.ac.uk>
2023-02-07 12:31:47 +0200

    Racer object movement with acceleration forwards and backwards(using matrix)

--- Game_Concepts_Hover_Racing.cpp
+++ Game_Concepts_Hover_Racing.cpp
@@ -9,10 +9,81 @@ using namespace std;
 
 //Constants
 float frameTime = 0.0f;
-
+float kRacerSpeed = 0.0f;
+float kRacerBackSpeed = 0.0f;
+float kMaxSpeed = 50.f;
+float kAcceleration = 70.0f;
+float kAirDrag = 1.0f;
+float kRacerRotateSpeed = 30.0f;
 //Functions
 
+//Structures
+typedef struct Racer {
+	IModel* model;
+	float matrix[4][4];
 
+	Racer(I3DEngine* myEngine, float x = 0.0f, float y = 0.0f, float z = 0.0f) {
+
+		model = myEngine->LoadMesh("Racer.x")->CreateModel(x, y, z);
+		model->GetMatrix(&matrix[0][0]);
+	}
+
+	void moveRight(bool move) {
+		if (move) {
+			//Rotate right and up. Slowly return back
+			model->RotateLocalZ(-kRacerRotateSpeed * frameTime);
+			model->RotateY(kRacerRotateSpeed * frameTime);
+			cout << matrix[0][0] << " " << matrix[0][1] << " " << matrix[0][2] << " " << endl;
+			cout << matrix[1][0] << " " << matrix[1][1] << " " << matrix[1][2] << " " << endl;
+			cout << matrix[2][0] << " " << matrix[2][1] << " " << matrix[2][2] << " " << endl;
+			cout << matrix[3][0] << " " << matrix[3][1] << " " << matrix[3][2] << " " << endl << endl;
+
+		}
+	}
+	void moveLeft(bool move) {
+		if (move) {
+			model->RotateLocalZ(kRacerRotateSpeed * frameTime);
+			model->RotateY(-kRacerRotateSpeed * frameTime);
+		}
+	}
+	void moveForward(bool move) {
+		if (move) {
+			matrix[3][2] += kRacerSpeed * frameTime;
+			if (kRacerSpeed < kMaxSpeed) {
+				kRacerSpeed += kAcceleration * frameTime;
+			}
+			model->SetMatrix(&matrix[0][0]);
+			cout << matrix[3][0] << " " << matrix[3][1] << " " << matrix[3][2] << " " << endl;
+		}
+		else {
+			if (kRacerSpeed > 0) {
+				matrix[3][2] += kRacerSpeed * frameTime;
+				kRacerSpeed -= kAcceleration * frameTime * kAirDrag;
+				model->SetMatrix(&matrix[0][0]);
+			}
+		}
+	}
+	void moveBackward(bool move) {
+		if (move) {
+			matrix[3][2] -= kRacerBackSpeed * frameTime;
+			if (kRacerBackSpeed < kMaxSpeed / 2) {
+				kRacerBackSpeed += kAcceleration * frameTime;
+			}
+			model->SetMatrix(&matrix[0][0]);
+		}
+		else {
+			if (kRacerBackSpeed > 0) {
+				matrix[3][2] -= kRacerBackSpeed * frameTime;
+				kRacerBackSpeed -= kAcceleration * frameTime * kAirDrag;
+				model->SetMatrix(&matrix[0][0]);
+			}
+		}
+	}
+
+	void Collide() {
+
+	}
+};
 
 void main()
 {
@@ -25,7 +96,7 @@ void main()
 	//myEngine->AddMediaFolder("./Assessment 2 Media");
 
 	/**** Set up your scene here ****/
-	ICamera* myCamera = myEngine->CreateCamera(kFPS);
+	ICamera* myCamera = myEngine->CreateCamera(kManual);
 
 
 	IMesh* skyMesh = myEngine->LoadMesh("Skybox.x");
@@ -49,30 +120,29 @@ void main()
 	IModel* isleModel4 = isleMesh->CreateModel(10, 0, 53);
 
 	
-	typedef struct Racer {
-		IModel* model;
-
-		Racer(I3DEngine* myEngine, float x = 0.0f, float y = 10.0f, float z = 0.0f){
-			
-			model = myEngine->LoadMesh("Racer.x")->CreateModel(x, y, z);
-		}
-	};
+	
 
 	
 	Racer player(myEngine);
 
 	
 
+	myEngine->StopMouseCapture();
+
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
 	{	
+		
 		frameTime = myEngine->Timer();
 		// Draw the scene
 		myEngine->DrawScene();
-
+		
 		/**** Update your scene each frame here ****/
 
-
+		player.moveRight(myEngine->KeyHeld(Key_D));
+		player.moveLeft(myEngine->KeyHeld(Key_A));
+		player.moveForward(myEngine->KeyHeld(Key_W));
+		player.moveBackward(myEngine->KeyHeld(Key_S));
 
 	}
 
